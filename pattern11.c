e05d452b43ff2de8f393ebdd535a547487ac426c
KalicharanSharma <79573910+KalicharanSharma@users.noreply.github.com>
2021-02-24 20:58:02 +0530

    Update pattern11.c

--- pattern11.c
+++ pattern11.c
@@ -7,7 +7,7 @@ int main(){
         for(j=0;j<n-i-1;j++)
             printf(" ");  
         for(j=0;j<2*i+1;j++)
-            printf("* ");
+            printf("*");
         printf("\n");  
     }return 0;
     

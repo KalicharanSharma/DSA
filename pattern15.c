e2cefc8ebfb097edf15baec0f8da341a31b398e9
KalicharanSharma <79573910+KalicharanSharma@users.noreply.github.com>
2021-02-24 21:25:34 +0530

    Update pattern15.c

--- pattern15.c
+++ pattern15.c
@@ -11,7 +11,7 @@ int main(){
         printf("\n");  
     }
     for(i=0;i<n;i++){
-        for(j=0;j<i;j++)
+        for(j=0;j<i+1;j++)
             printf(" ");  
         for(j=0;j<2*(n-1)-2*i-1;j++)
             printf("%d",j+1);
